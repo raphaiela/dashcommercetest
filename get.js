6e21b49e23e0920ea2ce334afe449eafa2d65aab
Raphaela <rapha.ela12@hotmail.com>
2022-07-03 19:53:19 -0300

    :art: Add the pages and JSON

--- get.js
+++ get.js
@@ -1,13 +1,13 @@
-
+const params = new URLSearchParams(window.location.search)
+var user = params.get('user')
+var password = params.get('password')
 var mensagens = {
     "mensagem_sucesso": "My full name is Raphaela Guimarães de Araújo dos Santos. I want this job!",
     "mensagem_falha": "Wrong credentials."
 }
-function validate() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    window.location.href = "dashcommercetest.html";
-    if (username == "dashcommerce" && password == "chooseMe") {
+function valida() {
+    var output = document.getElementById('output');
+    if (user == 'dashcommerce' && password == 'chooseMe') {
         output.innerHTML = mensagens.mensagem_sucesso;
     }
     else {
